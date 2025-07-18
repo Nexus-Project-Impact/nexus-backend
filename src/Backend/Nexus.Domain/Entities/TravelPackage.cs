68ce9226e20b6b723e3493d053e6700113728666
Julia Melo <julia.santos.de.melo@avanade.com>
2025-07-18 10:49:14 -0300

    correção travelpackage

--- src/Backend/Nexus.Domain/Entities/TravelPackage.cs
+++ src/Backend/Nexus.Domain/Entities/TravelPackage.cs
@@ -10,18 +10,17 @@ public class TravelPackage
 	public List<DateTime> AvailableDates { get; set; }
     public decimal Value { get; set; }
     public List<string> ImagesDestination { get; set; }
-
-    //public List<string> VideosDestination { get; set; }
-
 	public string HotelName { get; set; }
 	public string HotelAddress { get; set; }
     public TimeSpan TimeFlight { get; set; }
-
     public string Airline { get; set; }
 	public int HotelRating { get; set; }
-
+	public string DepartureAirport { get; set; }
+	public string ArrivalAirport { get; set; }
     public List<string> ImagesHotel { get; set; }
+
     //public List<string> VideosHotel { get; set; }
+    //public List<string> VideosDestination { get; set; }
 
 
 

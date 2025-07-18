c9f653253f2c7e69d2ffc0201b85c686bd528220
Julia Melo <julia.santos.de.melo@avanade.com>
2025-07-18 11:51:31 -0300

    correção TravelPackage e adição TravelPackageRepository

--- src/Backend/Nexus.Domain/Entities/TravelPackage.cs
+++ src/Backend/Nexus.Domain/Entities/TravelPackage.cs
@@ -7,20 +7,12 @@ public class TravelPackage
 	public string Description { get; set; }
 	public string Destination { get; set; }
 	public int Duration { get; set; }
-	public List<DateTime> AvailableDates { get; set; }
+	public DateTime DepartureDate { get; set; }
+	public DateTime ReturnDate { get; set; }
     public decimal Value { get; set; }
-    public List<string> ImagesDestination { get; set; }
-	public string HotelName { get; set; }
-	public string HotelAddress { get; set; }
-    public TimeSpan TimeFlight { get; set; }
-    public string Airline { get; set; }
-	public int HotelRating { get; set; }
-	public string DepartureAirport { get; set; }
-	public string ArrivalAirport { get; set; }
-    public List<string> ImagesHotel { get; set; }
+    
 
-    //public List<string> VideosHotel { get; set; }
-    //public List<string> VideosDestination { get; set; }
+    
 
 
 

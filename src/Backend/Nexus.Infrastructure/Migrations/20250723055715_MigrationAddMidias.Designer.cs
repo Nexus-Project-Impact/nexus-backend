fce255712b8d5efce998b166dc26cbe1ca09379e
José dos Santos Paiva <j.dos.santos.paiva@avanade.com>
2025-07-24 14:59:15 -0300

    feat: implementa serviço de e-mail, autenticação, registro de usuário, upload de mídia, seed de dados e padrão UnitOfWork

--- src/Backend/Nexus.Infrastructure/Migrations/20250723055715_MigrationAddMidias.Designer.cs
+++ src/Backend/Nexus.Infrastructure/Migrations/20250723055715_MigrationAddMidias.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nexus.Infrastructure.DataAccess;
 
@@ -11,9 +12,11 @@ using Nexus.Infrastructure.DataAccess;
 namespace Nexus.Infrastructure.Migrations
 {
     [DbContext(typeof(NexusDbContext))]
-    partial class NexusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250723055715_MigrationAddMidias")]
+    partial class MigrationAddMidias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

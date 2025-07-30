bd28ca6f13f0a68ab479aa4c762bfbe5d99e1e1f
Julia Melo <julia.santos.de.melo@avanade.com>
2025-07-30 10:39:56 -0300

    -add UseCase de GetByDepartureDate, GetByDestination, GetByValue

---
+++ src/Backend/Nexus.Application/UseCases/Packages/GetByDepartureDate/GetByDepartureDatePackageUseCase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Nexus.Communication.Responses;
+using Nexus.Domain.Entities;
+using Nexus.Domain.Repositories.Packages;
+
+namespace Nexus.Application.UseCases.Packages.GetByDepartureDate
+{
+    internal class GetByDepartureDatePackageUseCase : IGetByDepartureDatePackageUseCase
+    {
+        private readonly IPackageRepository<TravelPackage, int> _repository;
+        private readonly IMapper _mapper;
+
+        public GetByDepartureDatePackageUseCase(IPackageRepository<TravelPackage, int> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ResponsePackage?>> ExecuteGetByDepartureDate(DateTime initialDate, DateTime finalDate)
+        {
+            var packages =  await _repository.GetByDepartureDateAsync(initialDate, finalDate);
+
+            var packagesJson = _mapper.Map<IEnumerable<ResponsePackage?>>(packages);
+
+            return packagesJson;
+        }
+
+    }
+}

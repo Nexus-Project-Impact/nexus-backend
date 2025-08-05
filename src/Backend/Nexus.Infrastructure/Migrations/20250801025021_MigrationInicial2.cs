b6fab80531387a4631e91c13c4b78733e8710636
EduTakashi <eduardotaka2016@gmail.com>
2025-08-05 09:24:18 -0300

    Teste

---
+++ src/Backend/Nexus.Infrastructure/Migrations/20250801025021_MigrationInicial2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexus.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationInicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
